0f1f68fa8b3c091b203b90b31d800d08db269dee
NemissU <namphse171651@fpt.edu.vn>
2024-01-09 00:23:21 +0700

    Nam:Register user

---
+++ CleanArchitecture.Application/Users/CreateUser/RegisterRequestCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using CleanArchitecture.Application.Orders.CreateOrder;
+using CleanArchitecture.Domain.Entities;
+using CleanArchitecture.Domain.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CleanArchitecture.Domain.Entities;
+
+namespace CleanArchitecture.Application.Users.CreateUser
+{
+    public class RegisterRequestCommandHandler : IRequestHandler<RegisterRequestCommand, Guid>
+    {
+        IUserRepository _userRepository;
+        public RegisterRequestCommandHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+        public async Task<Guid> Handle(RegisterRequestCommand request, CancellationToken cancellationToken)
+        {
+            var User = new Domain.Entities.User
+            {
+                Id = Guid.NewGuid(),
+                Username = request.Username,
+                Password = request.Password,
+            };
+
+            _userRepository.Add(User);
+            await _userRepository.UnitOfWork.SaveChangesAsync(cancellationToken);
+            return User.Id;
+        }
+    }
+}
---
+++ CleanArchitecture/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using CleanArchitecture.Api.Controllers.ResponseTypes;
+using CleanArchitecture.Application.Common.Security;
+using CleanArchitecture.Application.Orders.CreateOrder;
+using CleanArchitecture.Application.Users.CreateUser;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
+
+namespace CleanArchitecture.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class UserController : ControllerBase
+    {
+        private readonly ISender _mediator;
+        public UserController(ISender mediator)
+        {                
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        }
+
+        [HttpPost("register")]
+        [Produces(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(typeof(JsonResponse<Guid>), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<JsonResponse<Guid>>> Register(
+                    [FromBody] RegisterRequestCommand command,
+                    CancellationToken cancellationToken = default)
+        {
+            var result = await _mediator.Send(command, cancellationToken);
+            //return CreatedAtAction(nameof(GetOrderById), new { id = result }, new JsonResponse<Guid>(result));
+            return CreatedAtAction(nameof(Register), new { id = result }, new JsonResponse<Guid>(result));
+        }
+    }
+}
