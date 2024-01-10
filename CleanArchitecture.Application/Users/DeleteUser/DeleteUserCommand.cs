a064264f8c6df0b155fd164432f2413a35e475ab
NemissU <namphse171651@fpt.edu.vn>
2024-01-10 09:52:14 +0700

    Nam:Delete User

---
+++ CleanArchitecture.Application/Users/DeleteUser/DeleteUserCommand.cs
@@ -0,0 +1,21 @@
+﻿using CleanArchitecture.Application.Common.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Application.Users.DeleteUser
+{
+    public class DeleteUserCommand : IRequest<Guid>, ICommand
+    {
+        public DeleteUserCommand(Guid userId)
+        {
+
+            UserId = userId;
+
+        }
+        public Guid UserId { get; set; }
+    }
+}
---
+++ CleanArchitecture.Application/Users/DeleteUser/DeleteUserCommandValidator.cs
@@ -0,0 +1,36 @@
+﻿using CleanArchitecture.Domain.Repositories;
+using FluentValidation;
+using FluentValidation.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Application.Users.DeleteUser
+{
+    public class DeleteUserCommandValidator : AbstractValidator<DeleteUserCommand>
+    {
+        IUserRepository _userRepository;
+        public DeleteUserCommandValidator(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+            ConfigureValidationRules();
+        }
+        private void ConfigureValidationRules()
+        {
+            RuleFor(v => v.UserId)
+                .NotEmpty().WithMessage("UserId is require")
+                .Must(CheckExist).WithMessage("UserId is not exist");
+        }
+        private bool CheckExist(Guid userId)
+        {
+            var user = _userRepository.FindAsync(x => x.Id == userId).Result;
+            if (user == null)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
