eb13157fba36c6752c701643601300562ebbbb0a
NemissU <namphse171651@fpt.edu.vn>
2024-01-10 09:31:37 +0700

    Nam: Update user

---
+++ CleanArchitecture.Application/Users/UpdateUser/UpdateUserCommand.cs
@@ -0,0 +1,21 @@
+﻿using CleanArchitecture.Application.Common.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Application.Users.UpdateUser
+{
+    public class UpdateUserCommand : IRequest<Guid>, ICommand
+    {
+        public UpdateUserCommand(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+        public string Username { get; private set; }
+        public string Password { get; set; }
+    }
+}
